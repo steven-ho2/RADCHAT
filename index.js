1def2947cbb56da166bbc9fe9cf914a9bba5734d
Steven <steven.ho2@ucalgary.ca>
2017-03-02 10:40:30 -0700

    Buts

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var msgLog = [];
+
+http.listen( port, function () {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// listen to 'chat' messages
+io.on('connection', function(socket){
+
+    socket.on('chat', function(data){
+	var time = getSentTime();
+	io.emit('chat', {'time': time, 'msg': data.msg});
+	msgLog.push({'sender': data.username, 'time': time, 'msg': data.msg});
+    });
+	
+    socket.on('name', function(){
+	var name = makeUserName();
+	io.emit('name', {'username' : name});
+
+    });
+	
+});
+
+//Get the time the message was sent
+function getSentTime(){
+	var d = new Date();
+	var str = d.toUTCString();
+	return str.match(/[0-9][0-9]:[0-9][0-9]/);
+}	
+
+
+//This will make the user name
+function makeUserName(){
+	var name  = randomEl(adjectives);
+	name += ' ' + randomEl(nouns);
+	return name;
+}
+
+//This is the name-gen source::: https://jsfiddle.net/katowulf/3gtDf/    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Get the random element
+function randomEl(list) {
+    var i = Math.floor(Math.random() * list.length);
+    return list[i];
+}
+
+//List of adjectives and nouns
+
+var adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
+
+var nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnomes", "glitter", "potato", "salad", "toejam", "curtains", "beets", "toilet", "exorcism", "stick figures", "mermaid eggs", "sea barnacles", "dragons", "jellybeans", "snakes", "dolls", "bushes", "cookies", "apples", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot air balloon", "praying mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman", "maintenance engineering", "mechanic", "miller", "moldmaker", "panel beater", "patternmaker", "plant operator", "plumber", "sawfiler", "shop foreman", "soaper", "stationary engineer", "wheelwright", "woodworkers"];
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ public/client.js
@@ -0,0 +1,25 @@
+// shorthand for $(document).ready(...)
+$(function() {
+    var socket = io();
+
+    var username = "";
+    socket.emit('name');
+    socket.on('name',function(data){
+	username = data.username
+    $('#username').text("Your username name is: " + username);
+    });    
+
+
+    $('form').submit(function(){
+	socket.emit('chat', {'name' : username, 'msg' : $('#m').val()});
+	$('#m').val('');
+	return false;
+    });
+    
+
+
+    socket.on('chat', function(data){
+	$('#messages').append($('<li>').text(data.time + ' ' + username + ": "+ data.msg));
+    });
+    
+});
